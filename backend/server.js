e3dc5e6f245bada28374bdacf7cb2a536d9230bd
DulranSam <dulransamarasinghe4@gmail.com>
2024-04-26 00:35:06 +0530

    shitt

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,17 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const db = require("./database/db")
-
-
+const db = require("./database/db");
 
 app.use(express.json());
-app.use(cors({origin:"*"}))
+app.use(cors({ origin: "*" }));
 
+app.use("/mains", require("./routes/main"));
+app.use("/users", require("./routes/users")); //done
+app.use("/feedbacks", require("./routes/feedback"));
+app.use("/searchs", require("./routes/searchs"));
 
-app.use("/mains",require("./routes/main"))
-app.use('/users',require("./routes/users")) //done
-app.use('/feedbacks',require("./routes/feedback"))
-app.use('/searchs',require("./routes/searchs"))
-
-
-app.listen(process.env.PORT,async ()=>{
-    await db();
-    console.log(`Servers up on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, async () => {
+  await db();
+  console.log(`Servers up on port ${process.env.PORT}`);
+});
